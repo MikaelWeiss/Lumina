42e19a2047fc284f230ca7c5430389d4f25d5e7a
Mikael Weiss <campingmikael@icloud.com>
2025-02-24 18:48:24 -0700

    Initial Commit

---
+++ Lumina/LuminaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuminaApp.swift
+//  Lumina
+//
+//  Created by Mikael Weiss on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct LuminaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LuminaTests/LuminaTests.swift
@@ -0,0 +1,17 @@
+//
+//  LuminaTests.swift
+//  LuminaTests
+//
+//  Created by Mikael Weiss on 2/24/25.
+//
+
+import Testing
+@testable import Lumina
+
+struct LuminaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
